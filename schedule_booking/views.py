313ef252309f4a8c420914116a3235f15f06150b
matteli <matthieu.nue@gmail.com>
2020-12-30 00:03:37 +0100

    test

--- schedule_booking/views.py
+++ schedule_booking/views.py
@@ -194,7 +194,7 @@ def scheduling_booking(request):
                             density=Cast(F("place__array"), FloatField())
                             / Cast(F("people") + 1, FloatField())
                         )
-                        .filter(density__lt=config["forbidden_level"])
+                        # .filter(density__lt=config["forbidden_level"])
                     )
                 if apps_invalid.count() == 0:
                     apps = Appointment.objects.filter(id__in=slots.values())
