3fcceabddd7b685e31561267a76891b7c5a0936b
matteli <matthieu.nue@gmail.com>
2021-01-20 17:23:30 +0100

    change admin

--- schedule_booking/admin.py
+++ schedule_booking/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import Place, Schedule, Appointment, Student, Config
 
-admin.site.register([Place, Schedule, Appointment, Student, Config])
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("lastname", "firstname", "email", "school")
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Schedule)
+admin.site.register(Place)
+admin.site.register(Appointment)
+admin.site.register(Config)
